ba1eb240bf25b1380087d14939f07981b9f936b0
asever77 <johyunmin77@gmail.com>
2020-05-29 15:04:26 +0900

    a

--- resources/js/ui.common.vanilla.js
+++ resources/js/ui.common.vanilla.js
@@ -38,29 +38,38 @@
             
         },
         menuAjax: function(){
-            console.log(11111111)
+
             doc.querySelectorAll('.dep-2-btn').forEach(function(btn, index){
-                btn.on('click.ajax', function(){
+                btn.off('click.ajax').on('click.ajax', function(){
+                    var href = this.getAttribute('data-href');
+
                     $plugins.uiAjax({ 
                         id: 'baseMain', 
-                        url: this.getAttribute('data-href'), 
+                        url: href, 
                         page: true, 
-                        callback: callbackJs
+                        callback: function(){
+                            callbackJs(href)
+                        }
                     });
                 });
             }); 
 
-            function callbackJs(){
+            function callbackJs(v){
                 if(typeof(history.pushState) == 'function') {
                    
                     //현재 주소를 가져온다.
                     var renewURL = location.href;
                     //현재 주소 중 page 부분이 있다면 날려버린다.
                     renewURL = renewURL.replace(/\&page=([0-9]+)/ig,'');
+                    renewURL = renewURL.split('/asever/');
+                    renewURL = renewURL[0];
                     
                     //새로 부여될 페이지 번호를 할당한다.
                     // page는 ajax에서 넘기는 page 번호를 변수로 할당해주거나 할당된 변수로 변경
-                    renewURL += 'loading/';
+                    renewURL = renewURL + v;
+
+                    console.log(renewURL);
+
                     
                     //페이지 갱신 실행!
                     history.pushState({ 'page_id': 1, 'user_id': 5 }, 'loading', renewURL);
